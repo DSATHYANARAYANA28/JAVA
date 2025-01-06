fe2d7f4fa585d57c8c652e50e6f06bc4313ba159
DSATHYANARAYANA28 <160825251+DSATHYANARAYANA28@users.noreply.github.com>
2025-01-06 09:24:18 +0530

    Update LCM.java
    completed

--- LCM.java
+++ LCM.java
@@ -1,5 +1,34 @@
-// getting input -  two int
+// logic:
+//getting input -  two int
 // finding big num
 // infinite loop
 // multiple -  brea - divisible by lowest value 
 // LCM
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+	    Scanner leo =  new Scanner(System.in);
+	    System.out.println("enter number 1:");
+	    int a = leo.nextInt();
+	    System.out.println("enter numer 2: ");
+	    int b =  leo.nextInt();
+	    
+	    int bigger =  a>b ? a:b ;
+	    int lcm = bigger;
+	    
+	    while(true){
+	        if (lcm % a  ==  0 && lcm % b == 0)
+	            break;
+	            else
+	           lcm += bigger;
+	           
+	        
+	        
+	    }
+	    System.out.println("LCM of " + a + " and " + b + " is: " + lcm); 
+	    
+	    }
+	    
+	}
